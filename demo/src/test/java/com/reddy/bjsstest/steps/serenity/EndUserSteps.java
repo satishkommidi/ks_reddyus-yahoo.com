f1118ae7c45b84c961278ba8cd830ea20e2dddd2
satishkommidi <43065494+satishkommidi@users.noreply.github.com>
2018-11-13 21:24:38 +0000

    Updated this file

--- demo/src/test/java/com/reddy/bjsstest/steps/serenity/EndUserSteps.java
+++ demo/src/test/java/com/reddy/bjsstest/steps/serenity/EndUserSteps.java
@@ -1,30 +1,18 @@
 package com.reddy.bjsstest.steps.serenity;
 
 import com.reddy.bjsstest.pages.AutomationPage;
-import com.reddy.bjsstest.pages.DictionaryPage;
 
 import com.reddy.bjsstest.pages.ProductDetailPage;
 import net.thucydides.core.annotations.Step;
-import net.thucydides.core.steps.ScenarioSteps;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
 public class EndUserSteps {
 
-    DictionaryPage dictionaryPage;
     AutomationPage automationPage;
     ProductDetailPage productPage;
 
-    @Step
-    public void enters(String keyword) {
-        dictionaryPage.enter_keywords(keyword);
-    }
-
-    @Step
-    public void starts_search() {
-        dictionaryPage.lookup_terms();
-    }
 
     @Step
     public void should_see_definition(String definition) {
@@ -32,26 +20,14 @@ public class EndUserSteps {
     }
 
     @Step
-    public void is_the_home_page() {
-        dictionaryPage.open();
+    public void is_on_automation_page() {
+        automationPage.open();
     }
 
     @Step
     public void looks_for(String term) {
         enters(term);
         starts_search();
-    }
-
-
-    @Step
-    public void is_on_automation_page() {
-        automationPage.open();
-    }
-
-    @Step
-    public void looks_for1(String term) {
-        enters1(term);
-        starts_search1();
     }
     @Step
     public void change_Size(String newSize) {
@@ -63,12 +39,12 @@ public class EndUserSteps {
         productPage.continueShopping();
     }
     @Step
-    public void enters1(String keyword) {
+    public void enters(String keyword) {
         automationPage.enter_keywords(keyword);
     }
 
     @Step
-    public void starts_search1() {
+    public void starts_search() {
         automationPage.lookup_terms();
     }
 
